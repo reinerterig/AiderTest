d75fdbf3b72c8a11da5f6fa9be9faadbbbc9cce5
reinerterig <54500223+reinerterig@users.noreply.github.com>
2023-07-07 14:38:15 +1000

    first commit

---
+++ AiderTest/AiderTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AiderTest
+//
+//  Created by reinert wasserman on 7/7/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
