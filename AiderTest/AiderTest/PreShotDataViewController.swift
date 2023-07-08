20a3be9d7f8367416c67799c839c26fd5b681c4f
reinerterig <54500223+reinerterig@users.noreply.github.com>
2023-07-08 14:52:40 +1000

    Set properties of UI elements and set up constraints in PreShotDataViewController.

--- AiderTest/AiderTest/PreShotDataViewController.swift
+++ AiderTest/AiderTest/PreShotDataViewController.swift
@@ -41,10 +41,55 @@ class PreShotDataViewController: UIViewController, UIPickerViewDataSource, UIPic
         grindPicker.delegate = self
         rpmPicker.dataSource = self
         rpmPicker.delegate = self
+        // Set properties of UI elements
+        doseLabel.text = "Dose: \(Dose)"
+        grindLabel.text = "Grind: \(Grind)"
+        rpmLabel.text = "RPM: \(RPM)"
+        preWetLabel.text = "PreWet: \(PreWet ? "On" : "Off")"
+        doseButton.setTitle("Set", for: .normal)
+        nextButton.setTitle("Next", for: .normal)
+        
+        // Set up constraints
+        // Note: You'll need to replace these placeholder constraints with the actual constraints you want
+        doseLabel.translatesAutoresizingMaskIntoConstraints = false
+        grindLabel.translatesAutoresizingMaskIntoConstraints = false
+        rpmLabel.translatesAutoresizingMaskIntoConstraints = false
+        preWetLabel.translatesAutoresizingMaskIntoConstraints = false
+        doseButton.translatesAutoresizingMaskIntoConstraints = false
+        grindPicker.translatesAutoresizingMaskIntoConstraints = false
+        rpmPicker.translatesAutoresizingMaskIntoConstraints = false
+        preWetSwitch.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            grindPicker.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            grindPicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            doseButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            rpmPicker.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+//            // Dose button is 20 points from the Grind picker's leading edge
+            doseButton.trailingAnchor.constraint(equalTo: grindPicker.leadingAnchor, constant: -20),
+//            // RPM picker is 20 points from the Grind picker's trailing edge
+            rpmPicker.leadingAnchor.constraint(equalTo: grindPicker.trailingAnchor, constant: 20),
+//            // All labels are 20 points above their corresponding buttons or pickers
+            doseLabel.bottomAnchor.constraint(equalTo: doseButton.topAnchor, constant: -20),
+            grindLabel.bottomAnchor.constraint(equalTo: grindPicker.topAnchor, constant: -20),
+            rpmLabel.bottomAnchor.constraint(equalTo: rpmPicker.topAnchor, constant: -20),
+            preWetLabel.trailingAnchor.constraint(equalTo: preWetSwitch.leadingAnchor, constant: -20),
+//            // PreWet switch is 20 points below the Grind picker and centered in the screen
+            preWetSwitch.topAnchor.constraint(equalTo: grindPicker.bottomAnchor, constant: 20),
+            preWetSwitch.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            // PreWet label is 20 points to the PreWet switch's leading edge
+//            // Next button is in the bottom right corner of the screen
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+        ])
     }
 
     // MARK: - UIPickerViewDataSource
 
+   
+}
+extension PreShotDataViewController {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -74,55 +119,5 @@ class PreShotDataViewController: UIViewController, UIPickerViewDataSource, UIPic
             return nil
         }
     }
-
-        // Set properties of UI elements
-        doseLabel.text = "Dose: \(Dose)"
-        grindLabel.text = "Grind: \(Grind)"
-        rpmLabel.text = "RPM: \(RPM)"
-        preWetLabel.text = "PreWet: \(PreWet ? "On" : "Off")"
-        doseButton.setTitle("Set", for: .normal)
-        nextButton.setTitle("Next", for: .normal)
-
-        // Set up constraints
-        // Note: You'll need to replace these placeholder constraints with the actual constraints you want
-        doseLabel.translatesAutoresizingMaskIntoConstraints = false
-        grindLabel.translatesAutoresizingMaskIntoConstraints = false
-        rpmLabel.translatesAutoresizingMaskIntoConstraints = false
-        preWetLabel.translatesAutoresizingMaskIntoConstraints = false
-        doseButton.translatesAutoresizingMaskIntoConstraints = false
-        grindPicker.translatesAutoresizingMaskIntoConstraints = false
-        rpmPicker.translatesAutoresizingMaskIntoConstraints = false
-        preWetSwitch.translatesAutoresizingMaskIntoConstraints = false
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
-
-        // Add constraints here
-       
-            
-        NSLayoutConstraint.activate([
-            // Dose button is 20 points from the Grind picker's leading edge
-            doseButton.leadingAnchor.constraint(equalTo: grindPicker.leadingAnchor, constant: -20),
-            // RPM picker is 20 points from the Grind picker's trailing edge
-            rpmPicker.leadingAnchor.constraint(equalTo: grindPicker.trailingAnchor, constant: 20),
-            // All labels are 20 points above their corresponding buttons or pickers
-            doseLabel.bottomAnchor.constraint(equalTo: doseButton.topAnchor, constant: -20),
-            grindLabel.bottomAnchor.constraint(equalTo: grindPicker.topAnchor, constant: -20),
-            rpmLabel.bottomAnchor.constraint(equalTo: rpmPicker.topAnchor, constant: -20),
-            preWetLabel.bottomAnchor.constraint(equalTo: preWetSwitch.topAnchor, constant: -20),
-            // PreWet switch is 20 points below the Grind picker and centered in the screen
-            preWetSwitch.topAnchor.constraint(equalTo: grindPicker.bottomAnchor, constant: 20),
-            preWetSwitch.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            // PreWet label is 20 points to the PreWet switch's leading edge
-            preWetLabel.trailingAnchor.constraint(equalTo: preWetSwitch.leadingAnchor, constant: -20),
-            // Next button is in the bottom right corner of the screen
-            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
-            // centered horizontally in the screen
-            grindPicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            rpmPicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
-            // Grind picker is centered vertically in the screen
-            grindPicker.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-    }
+    
 }
