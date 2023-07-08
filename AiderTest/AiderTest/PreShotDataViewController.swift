ba3f09d87a857620d762acb8acfbe3a4b6d02a4e
reinerterig <54500223+reinerterig@users.noreply.github.com>
2023-07-08 14:27:59 +1000

    aider: Set up pickers with correct values for grind and rpm.
    # Context:
    USER: thats better can you set up my pickers so they have all the correct values? grind should range from 0-30 in 0.5 intervals and rpm should be 100-1800 in 100 intervals
    ASSISTANT: Sure, let's set up the pickers. We'll need to make `PreShotDataViewController` conform to the `UIPickerViewDataSource` and `UIPickerViewDelegate` protocols, and implement the required methods to provide the data for the pickers and handle the selection events.
    
    ```swift
    AiderTest/AiderTest/PreShotDataViewController.swift
    <<<<<<< ORIGINAL
    class PreShotDataViewController: UIViewController {
    =======
    class PreShotDataViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
    >>>>>>> UPDATED
    
    ```
    
    Next, we'll set the data source and delegate of the pickers to `self`, and implement the `numberOfComponents(in:)`, `pickerView(_:numberOfRowsInComponent:)`, and `pickerView(_:titleForRow:forComponent:)` methods to provide the data for the pickers.
    
    ```swift
    AiderTest/AiderTest/PreShotDataViewController.swift
    <<<<<<< ORIGINAL
        func setupUI() {
            // Add UI elements to the view
            view.addSubview(doseLabel)
            view.addSubview(grindLabel)
            view.addSubview(rpmLabel)
            view.addSubview(preWetLabel)
            view.addSubview(doseButton)
            view.addSubview(grindPicker)
            view.addSubview(rpmPicker)
            view.addSubview(preWetSwitch)
            view.addSubview(nextButton)
    =======
        func setupUI() {
            // Add UI elements to the view
            view.addSubview(doseLabel)
            view.addSubview(grindLabel)
            view.addSubview(rpmLabel)
            view.addSubview(preWetLabel)
            view.addSubview(doseButton)
            view.addSubview(grindPicker)
            view.addSubview(rpmPicker)
            view.addSubview(preWetSwitch)
            view.addSubview(nextButton)
    
            // Set the data source and delegate of the pickers
            grindPicker.dataSource = self
            grindPicker.delegate = self
            rpmPicker.dataSource = self
            rpmPicker.delegate = self
        }
    
        // MARK: - UIPickerViewDataSource
    
        func numberOfComponents(in pickerView: UIPickerView) -> Int {
            return 1
        }
    
        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
            if pickerView == grindPicker {
                // Grind ranges from 0 to 30 in 0.5 intervals, so there are 61 values
                return 61
            } else if pickerView == rpmPicker {
                // RPM ranges from 100 to 1800 in 100 intervals, so there are 18 values
                return 18
            } else {
                return 0
            }
        }
    
        // MARK: - UIPickerViewDelegate
    
        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
            if pickerView == grindPicker {
                // Grind ranges from 0 to 30 in 0.5 intervals
                return String(format: "%.1f", Double(row) * 0.5)
            } else if pickerView == rpmPicker {
                // RPM ranges from 100 to 1800 in 100 intervals
                return String(row * 100 + 100)
            } else {
                return nil
            }
        }
    >>>>>>> UPDATED
    ```
    
    This will set up the Grind picker with values ranging from 0 to 30 in 0.5 intervals, and the RPM picker with values ranging from 100 to 1800 in 100 intervals. Let me know if you have any questions or if you'd like me to proceed with the next steps.

--- AiderTest/AiderTest/PreShotDataViewController.swift
+++ AiderTest/AiderTest/PreShotDataViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class PreShotDataViewController: UIViewController {
+class PreShotDataViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     var Dose: Double = 0.0
     var Grind: Double = 17.0
     var RPM: Double = 100.0
@@ -35,6 +35,45 @@ class PreShotDataViewController: UIViewController {
         view.addSubview(rpmPicker)
         view.addSubview(preWetSwitch)
         view.addSubview(nextButton)
+
+        // Set the data source and delegate of the pickers
+        grindPicker.dataSource = self
+        grindPicker.delegate = self
+        rpmPicker.dataSource = self
+        rpmPicker.delegate = self
+    }
+
+    // MARK: - UIPickerViewDataSource
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == grindPicker {
+            // Grind ranges from 0 to 30 in 0.5 intervals, so there are 61 values
+            return 61
+        } else if pickerView == rpmPicker {
+            // RPM ranges from 100 to 1800 in 100 intervals, so there are 18 values
+            return 18
+        } else {
+            return 0
+        }
+    }
+
+    // MARK: - UIPickerViewDelegate
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == grindPicker {
+            // Grind ranges from 0 to 30 in 0.5 intervals
+            return String(format: "%.1f", Double(row) * 0.5)
+        } else if pickerView == rpmPicker {
+            // RPM ranges from 100 to 1800 in 100 intervals
+            return String(row * 100 + 100)
+        } else {
+            return nil
+        }
+    }
 
         // Set properties of UI elements
         doseLabel.text = "Dose: \(Dose)"
